39141d21cd0fb048ae0c871e9310ff8619ba1da2
Jan Vojt <jan@vojt.net>
2014-08-21 22:26:24 +0200

    Added Main class.

---
+++ src/main/java/net/vojt/calculator/Calculator.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2014 janvojt.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+package net.vojt.calculator;
+
+/**
+ * The main class that launches the application.
+ *
+ * @author janvojt
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Calculator!");
+    }
+    
+}
